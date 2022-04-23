1e388160ec6c6c85a52e85e0ae6186445a07af93
Erickype <erickype@hotmail.com>
2022-04-23 12:44:36 -0500

    Operaciones de la barca

--- BusquedasNoInformadas/Operaciones.cs
+++ BusquedasNoInformadas/Operaciones.cs
@@ -69,11 +69,63 @@ namespace BusquedasNoInformadas
                 isla.canibales--;
             }
         }
-        public void restar1M(Isla isla) { }
-        public void restar2M(Isla isla) { }
-        public void restar1C(Isla isla) { }
-        public void restar2C(Isla isla) { }
-        public void restar1M1C(Isla isla) { }
+        public void restar1M(Isla isla) 
+        {
+            if (isla.barca == null)
+                return;
+
+            if (isla.barca.misioneros > 1)
+            {
+                isla.barca.misioneros--;
+                isla.misioneros++;
+            }
+        }
+        public void restar2M(Isla isla)
+        {
+            if (isla.barca == null)
+                return;
+
+            if (isla.barca.misioneros == 2)
+            {
+                isla.barca.misioneros -= 2;
+                isla.misioneros += 2;
+            }
+        }
+        public void restar1C(Isla isla)
+        {
+            if (isla.barca == null)
+                return;
+
+            if (isla.barca.canibales > 1)
+            {
+                isla.barca.canibales--;
+                isla.canibales++;
+            }
+        }
+        public void restar2C(Isla isla)
+        {
+            if (isla.barca == null)
+                return;
+
+            if (isla.barca.canibales == 2)
+            {
+                isla.barca.canibales -= 2;
+                isla.canibales += 2;
+            }
+        }
+        public void restar1M1C(Isla isla)
+        {
+            if (isla.barca == null)
+                return;
+
+            if (isla.barca.misioneros == 1 && isla.barca.canibales == 1)
+            {
+                isla.barca.misioneros--;
+                isla.barca.canibales--;
+                isla.misioneros++;
+                isla.canibales++;
+            }
+        }
 
     }
 }
